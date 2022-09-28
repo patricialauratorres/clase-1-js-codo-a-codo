2300827f27ca68d78a336a346d8fed8fb4413e54
patricialauratorres <patricialauratorres@hotmail.com>
2022-09-28 12:09:28 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//alert ("hola java script");
+// para comentarios
+/* para varias lineas*/
+
+//variables
+var nombre="nombre";
+let apellido ="pepe";
+const edad = 30;
+
+var nombreApellido =" german settino"; 
+//alert ("nombreApellido" + nombreApellido);
+
+console.log(nombreApellido);
+
+let edad1= 30;
+let hijos = false;
+let sueldo =1500.5;
+let casado = true;
+console.log(nombre);
+
+console.log(typeof nombre);
+console.log(typeof edad);
